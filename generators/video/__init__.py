8f983c2ca52820c6e623b079d528f9de7b2338d6
Toyoharukohyama <toyoharukohyama@example.com>
2026-07-26 20:22:04 +0900

    feat: add bounded timeline assembly generator

--- generators/video/__init__.py
+++ generators/video/__init__.py
@@ -1,5 +1,11 @@
 """Video generator package."""
 
+from .assembly import ASSEMBLY_OUTPUT_FORMATS, ASSEMBLY_TASK_TYPE, AssemblyGenerator
 from .generator import VideoGenerator
 
-__all__ = ["VideoGenerator"]
+__all__ = [
+    "ASSEMBLY_OUTPUT_FORMATS",
+    "ASSEMBLY_TASK_TYPE",
+    "AssemblyGenerator",
+    "VideoGenerator",
+]
